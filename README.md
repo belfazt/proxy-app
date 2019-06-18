4177e5c01e29d997dc79e705694af64b07ccbbfe
Diego Camargo <dicamargov@gmail.com>
2019-06-18 18:04:13 -0500

    Add support for environment variables

---
+++ README.md
@@ -0,0 +1,10 @@
+# Proxy App
+
+## Configuring
+
+### Sample .env
+
+```env
+PORT=8080
+```
+
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "github.com/belfazt/proxy-app/api/handlers"
+import (
+	handlers "github.com/belfazt/proxy-app/api/handlers"
+	utils "github.com/belfazt/proxy-app/api/utils"
+)
 
 func main() {
 	/*
 		Router Iris
 		ENV Vars
 	*/
+
+	utils.LoadEnv()
 
 	handlers.HandleRedirection()
 }
