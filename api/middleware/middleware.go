775cd8cbfa681b575ce0919cc2a866a29a26b2fe
Diego Camargo <dicamargov@gmail.com>
2019-06-27 15:04:33 -0500

    Use a file to configure the prioritization instead of having it hardcoded

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -1,8 +1,16 @@
 package middleware
 
 import (
-	"fmt"
+	"bufio"
 	"github.com/kataras/iris"
+	"github.com/timtadh/data-structures/hashtable"
+	"github.com/timtadh/data-structures/types"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 type QueueNode struct {
@@ -11,45 +19,81 @@ type QueueNode struct {
 	Priority int
 }
 
+type PrioNode struct {
+	Weight   int
+	Priority int
+}
+
 var Queue []*QueueNode
+var PriorityConfig hashtable.Hash
+var priorityConfigInitialSize int
 
-type Repository interface {
-	Read() []*QueueNode
+func Init() {
+	priorityConfigInitialSize = 10
+	PriorityConfig = readPriorityConfig()
 }
 
-func (q *QueueNode) Read() []*QueueNode {
-	return MockQueue()
-}
-
-func MockQueue() []*QueueNode {
-	return []*QueueNode{
-		{
-			Domain:   "alpha",
-			Weight:   5,
-			Priority: 5,
-		},
-		{
-			Domain:   "beta",
-			Weight:   5,
-			Priority: 5,
-		},
-		{
-			Domain:   "omega",
-			Weight:   5,
-			Priority: 1,
-		},
+func Handler(c iris.Context) {
+	var domain = c.GetHeader("domain")
+	if len(domain) == 0 {
+		c.JSON(iris.Map{"status": 400, "result": "error"})
+		return
 	}
+
+	if !PriorityConfig.Has(types.String(domain)) {
+		c.JSON(iris.Map{"status": 400, "result": "unexpected domain"})
+		return
+	}
+
+	var node = QueueNode{}
+
+	var hashNode, err = PriorityConfig.Get(types.String(domain))
+
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(iris.Map{"status": 500, "result": "something went wrong"})
+		return
+	}
+
+	node.Domain = domain
+	node.Weight = hashNode.(PrioNode).Weight
+	node.Priority = hashNode.(PrioNode).Priority
+
+	Queue = append(Queue, &node)
+
+	c.Next()
 }
 
 func Init() {
 	Queue = append(Queue, &QueueNode{})
 }
 
-func Handler(c iris.Context) {
-	var domain = c.GetHeader("domain")
-	var repository Repository
-	repository = &QueueNode{}
-	fmt.Println(domain)
-	fmt.Println(repository.Read())
-	c.Next()
+func readPriorityConfig() hashtable.Hash {
+	var path, _ = filepath.Abs("")
+	file, err := os.Open(path + "/api/middleware/domain.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	var scanner = bufio.NewScanner(file)
+
+	var hash = *hashtable.NewHashTable(priorityConfigInitialSize)
+
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			continue
+		}
+
+		var key = types.String(scanner.Text())
+		var node = PrioNode{}
+		scanner.Scan()
+		node.Weight, _ = strconv.Atoi(strings.Split(scanner.Text(), ":")[1])
+		scanner.Scan()
+		node.Priority, _ = strconv.Atoi(strings.Split(scanner.Text(), ":")[1])
+
+		hash.Put(key, node)
+	}
+
+	return hash
 }
